ef5194aaed9748e609ad199960a60b09e190479a
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-28 00:01:54 +0530

    Refactored auth.py for improved structure and clarity; removed unused imports and ensured consistent formatting.

--- backend/src/schema/auth.py
+++ backend/src/schema/auth.py
@@ -1,43 +1,39 @@
-from datetime import datetime, timezone
-from typing import Optional
+from typing import List
 from uuid import UUID
-
-from fastapi import File, Form, UploadFile
 from pydantic import BaseModel, EmailStr, Field
-
-from backend.src.model.enum import UserTitle
-
+from fastapi import File, UploadFile
+from src.model.enum import UserTitle
+ 
+ 
 class Certificate(BaseModel):
-    user_certificate : UploadFile = File() 
-
+    user_certificate: UploadFile = File()
+ 
+ 
 class RegisterUser(Certificate, BaseModel):
-    user_title : UserTitle
-    user_first_name : str 
-    user_last_name : str 
-    user_email : EmailStr 
-    user_password : str = Field(...,min_length=8, max_length=12)
-    user_confirm_password : str = Field(...,min_length=8, max_length=12)
-    user_degree : str 
-    user_passing_year : int
-
-
-
+    user_title: UserTitle
+    user_first_name: str
+    user_last_name: str
+    user_email: EmailStr
+    user_password: str = Field(..., min_length=8, max_length=12)
+    user_confirm_password: str = Field(..., min_length=8, max_length=12)
+    user_degree: str
+    user_passing_year: int
+ 
+ 
 class Register(RegisterUser):
     pass
-
-class ProductUploadSchema(BaseModel):
-    name: str
-    price: float
-    description: str | None = None
-    profile_image: UploadFile
-
+ 
+ 
 class LoginUser(BaseModel):
-    user_email : EmailStr
-    user_password : str = Field(min_length=8, max_length=12)
-
+    user_email: EmailStr
+    user_password: str = Field(min_length=8, max_length=12)
+ 
+ 
 class LoginResponse(BaseModel):
-    user_role : str
-    user_id : UUID
-    access_token : str
-    refresh_token : str
-    token_type : str = Field(default="Bearer")
+    user_roles: List[str]           
+    active_role: str  
+    user_id: UUID
+    access_token: str
+    refresh_token: str
+    token_type: str = Field(default="Bearer")
+ 
